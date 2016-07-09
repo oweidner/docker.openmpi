a627ad34eafcc746fa7c913fe1e3a488f501e1a4
ocramz <zocca.marco@gmail.com>
2016-07-09 11:44:53 +0200

    all mpi4py tests in a single py file

--- mpi4py_benchmarks/all_tests.py
+++ mpi4py_benchmarks/all_tests.py
@@ -33,9 +33,9 @@ def pprint(str="", end="\n", comm=MPI.COMM_WORLD):
 #=============================================================================
 # Main
 
-def mvp_main(size=10000, iter=2000):
+def mvp_main(size=10000, iter=200):
     # size = 10000           # length of vector v
-    # iter = 2000            # number of iterations to run
+    # iter = 200             # number of iterations to run
 
     counter = 0
 
@@ -143,6 +143,151 @@ def osu_latency(
             latency = (t_end - t_start) * 1e6 / (2 * loop)
             print ('%-10d%20.2f' % (size, latency))
 
+def osu_bibw(
+    BENCHMARH = "MPI Bi-Directional Bandwidth Test",
+    skip = 10,
+    loop = 100,
+    window_size = 64,
+    skip_large = 2,
+    loop_large = 20,
+    window_size_large = 64,
+    large_message_size = 8192,
+    MAX_MSG_SIZE = 1<<22,
+    ):
+
+    comm = MPI.COMM_WORLD
+    myid = comm.Get_rank()
+    numprocs = comm.Get_size()
+
+    if numprocs != 2:
+        if myid == 0:
+            errmsg = "This test requires exactly two processes"
+        else:
+            errmsg = None
+        raise SystemExit(errmsg)
+
+    s_buf = allocate(MAX_MSG_SIZE)
+    r_buf = allocate(MAX_MSG_SIZE)
+
+    if myid == 0:
+        print ('# %s' % (BENCHMARH,))
+    if myid == 0:
+        print ('# %-8s%20s' % ("Size [B]", "Bandwidth [MB/s]"))
+
+    message_sizes = [2**i for i in range(30)]
+    for size in message_sizes:
+        if size > MAX_MSG_SIZE:
+            break
+        if size > large_message_size:
+            skip = skip_large
+            loop = loop_large
+            window_size = window_size_large
+
+        iterations = list(range(loop+skip))
+        window_sizes = list(range(window_size))
+        s_msg = [s_buf, size, MPI.BYTE]
+        r_msg = [r_buf, size, MPI.BYTE]
+        send_request = [MPI.REQUEST_NULL] * window_size
+        recv_request = [MPI.REQUEST_NULL] * window_size
+        #
+        comm.Barrier()
+        if myid == 0:
+            for i in iterations:
+                if i == skip:
+                    t_start = MPI.Wtime()
+                for j in window_sizes:
+                    recv_request[j] = comm.Irecv(r_msg, 1, 10)
+                for j in window_sizes:
+                    send_request[j] = comm.Isend(s_msg, 1, 100)
+                MPI.Request.Waitall(send_request)
+                MPI.Request.Waitall(recv_request)
+            t_end = MPI.Wtime()
+        elif myid == 1:
+            for i in iterations:
+                for j in window_sizes:
+                    recv_request[j] = comm.Irecv(r_msg, 0, 100)
+                for j in window_sizes:
+                    send_request[j] = comm.Isend(s_msg, 0, 10)
+                MPI.Request.Waitall(send_request)
+                MPI.Request.Waitall(recv_request)
+        #
+        if myid == 0:
+            MB = size / 1e6 * loop * window_size
+            s = t_end - t_start
+            print ('%-10d%20.2f' % (size, MB/s))
+
+            
+def osu_bw(
+    BENCHMARH = "MPI Bandwidth Test",
+    skip = 10,
+    loop = 100,
+    window_size = 64,
+    skip_large = 2,
+    loop_large = 20,
+    window_size_large = 64,
+    large_message_size = 8192,
+    MAX_MSG_SIZE = 1<<22,
+    ):
+
+    comm = MPI.COMM_WORLD
+    myid = comm.Get_rank()
+    numprocs = comm.Get_size()
+
+    if numprocs != 2:
+        if myid == 0:
+            errmsg = "This test requires exactly two processes"
+        else:
+            errmsg = None
+        raise SystemExit(errmsg)
+
+    s_buf = allocate(MAX_MSG_SIZE)
+    r_buf = allocate(MAX_MSG_SIZE)
+
+    if myid == 0:
+        print ('# %s' % (BENCHMARH,))
+    if myid == 0:
+        print ('# %-8s%20s' % ("Size [B]", "Bandwidth [MB/s]"))
+
+    message_sizes = [2**i for i in range(30)]
+    for size in message_sizes:
+        if size > MAX_MSG_SIZE:
+            break
+        if size > large_message_size:
+            skip = skip_large
+            loop = loop_large
+            window_size = window_size_large
+
+        iterations = list(range(loop+skip))
+        window_sizes = list(range(window_size))
+        requests = [MPI.REQUEST_NULL] * window_size
+        #
+        comm.Barrier()
+        if myid == 0:
+            s_msg = [s_buf, size, MPI.BYTE]
+            r_msg = [r_buf,    4, MPI.BYTE]
+            for i in iterations:
+                if i == skip:
+                    t_start = MPI.Wtime()
+                for j in window_sizes:
+                    requests[j] = comm.Isend(s_msg, 1, 100)
+                MPI.Request.Waitall(requests)
+                comm.Recv(r_msg, 1, 101)
+            t_end = MPI.Wtime()
+        elif myid == 1:
+            s_msg = [s_buf,    4, MPI.BYTE]
+            r_msg = [r_buf, size, MPI.BYTE]
+            for i in iterations:
+                for j in window_sizes:
+                    requests[j] = comm.Irecv(r_msg, 0, 100)
+                MPI.Request.Waitall(requests)
+                comm.Send(s_msg, 0, 101)
+        #
+        if myid == 0:
+            MB = size / 1e6 * loop * window_size
+            s = t_end - t_start
+            print ('%-10d%20.2f' % (size, MB/s))
+
+            
 
 def allocate(n):
     try:
@@ -160,3 +305,5 @@ def allocate(n):
 if __name__ == '__main__':
     mvp_main()
     osu_latency()
+    osu_bw()
+    osu_bibw()
