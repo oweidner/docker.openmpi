73096653d306d6f365935b11f50a84e09557a5ae
ocramz <zocca.marco@gmail.com>
2016-07-09 09:00:55 +0200

    typo

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ NNODES=$1
 pwd
 ls -lsA
 
-mpirun -n ${NNODES} python matrix_vector_product.py
-mpirun -n ${NNODES} python osu_bibw.py
-mpirun -n ${NNODES} python osu_bw.py
-mpirun -n ${NNODES} python osu_latency.py
+mpirun -n ${NNODES} python ${HOME}/mpi4py_benchmarks/matrix_vector_product.py
+mpirun -n ${NNODES} python ${HOME}/mpi4py_benchmarks/osu_bibw.py
+mpirun -n ${NNODES} python ${HOME}/mpi4py_benchmarks/osu_bw.py
+mpirun -n ${NNODES} python ${HOME}/mpi4py_benchmarks/osu_latency.py
