350e93a39abbba394215734d4e10b7fd81fb9dcb
ocramz <zocca.marco@gmail.com>
2016-07-09 09:56:20 +0200

    typo

--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,7 @@ cd ${HOME}/mpi4py_benchmarks/
 pwd
 ls -lsA
 
-mpirun -n ${NNODES} python matrix_vector_product.py
-mpirun -n ${NNODES} python osu_bibw.py
-mpirun -n ${NNODES} python osu_bw.py
-mpirun -n ${NNODES} python osu_latency.py
+mpirun -n ${NNODES} python ./matrix_vector_product.py
+mpirun -n ${NNODES} python ./osu_bibw.py
+mpirun -n ${NNODES} python ./osu_bw.py
+mpirun -n ${NNODES} python ./osu_latency.py
